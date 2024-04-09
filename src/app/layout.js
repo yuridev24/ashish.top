079f433bd782c9bf4df765b3fce404b56a9a82c7
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-09 17:35:08 +0530

    add: more ogs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,44 @@
 import "./globals.css";
-import Head from 'next/head';
+// import { Metadata } from "next";
 import {inter, poppins} from "./fonts.js"
 
 export const metadata = {
   title: "Ashish Agarwal",
   description:
     "I'm a programmer and a book author. I build things for the web at a young age.",
-  image: "/meta.png",
-  themeColor: "rgb(18, 18, 18)",
+  keywords: [
+    "Next.js",
+    "React",
+    "Portfolio",
+    "JavaScript",
+    "Python",
+    "Programmer",
+    "Young",
+    "Ashish",
+    "Computer Boy",
+    "Aligarh",
+  ],
+  creator: "Ashish Agarwal",
+  openGraph: {
+    title: "Ashish Agarwal",
+    description:
+      "I'm a programmer and a book author. I build things for the web at a young age.",
+    url: "https://ashishagarwal.is-a.dev",
+    siteName: "Ashish Agarwal",
+    images: [
+      {
+        url: "https://ashishagarwal.is-a.dev/meta.png",
+        width: 1366,
+        height: 443,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:width" content="1366" />{" "}
-        <meta property="og:image:height" content="463" />{" "}
-        <meta name="theme-color" content={metadata.themeColor} />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
