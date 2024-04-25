b7487a81219ea68ea65277ed49e3fb672e96d0ce
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-25 20:04:06 +0530

    ...

--- src/app/blog/[postSlug]/page.js
+++ src/app/blog/[postSlug]/page.js
@@ -1,5 +1,11 @@
 import { Heading } from "./components/Heading";
 
+export const generateStaticParams = () => {
+  return [
+    { postSlug: "first" },
+  ];
+};
+
 export const Post = () => {
   return (
     <>
