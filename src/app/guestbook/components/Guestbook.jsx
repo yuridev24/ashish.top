093028905200188c8bee3108691e8a247a524719
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-27 01:25:43 +0530

    change 1 to A

--- src/app/guestbook/components/Guestbook.jsx
+++ src/app/guestbook/components/Guestbook.jsx
@@ -48,23 +48,23 @@ export const Guestbook = () => {
     if (seconds < 60) {
       return "Just now";
     } else if (minutes === 1) {
-      return "1 minute ago";
+      return "A minute ago";
     } else if (minutes < 60) {
       return `${minutes} minutes ago`;
     } else if (hours === 1) {
-      return "1 hour ago";
+      return "A hour ago";
     } else if (hours < 24) {
       return `${hours} hours ago`;
     } else if (days === 1) {
-      return "1 day ago";
+      return "A day ago";
     } else if (days < 30) {
       return `${days} days ago`;
     } else if (months === 1) {
-      return "1 month ago";
+      return "A month ago";
     } else if (months < 12) {
       return `${months} months ago`;
     } else if (years === 1) {
-      return "1 year ago";
+      return "A year ago";
     } else {
       return `${years} years ago`;
     }
