ebd635a5bae51d7316a16faac59e9823071163c1
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-05-03 17:10:45 +0530

    Oh the export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
