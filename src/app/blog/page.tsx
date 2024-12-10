bfcd15a23679ddcf9bd0663812ce10e96a6aa075
Ashish Agarwal <ashish.agarwals.2022@gmail.com>
2024-12-10 15:06:11 +0530

    remove search feature temporarily

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,12 @@
-import Search from "@/components/Search";
+// import Search from "@/components/Search";
 import {NewsLetter} from "./components/NewsLetter";
-import { Posts } from "./components/Posts";
+// import { Posts } from "./components/Posts";
 
 export default async function Blog() {
   return (
     <>
       <NewsLetter />
-      <Search />
+      {/* <Search /> */}
       {/* <Posts /> */}
     </>
   );
