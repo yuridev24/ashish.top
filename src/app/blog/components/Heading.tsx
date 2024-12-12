d4a2f8b1ad9df044904eb4b806fccdcb19da57a3
Ashish Agarwal <ashish.agarwals.2022@gmail.com>
2024-12-12 23:09:45 +0530

    permanent removal of hashnode headless blog due to the vast universe of bugs and moths

--- src/app/blog/components/Heading.tsx
+++
@@ -1,8 +0,0 @@
-export const Heading = () => {
-  return (
-    <>
-      <div className="text-3xl md:text-5xl font-semibold text-primary-500">My Blog</div>
-      <p className="mt-2">Here you see all my latest blog posts. Remember to not miss them!</p>
-    </>
-  );
-}
